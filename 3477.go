0a661c593577b4a7f63790db4de6a907eaf073f6
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2025-08-06 10:56:57 +0300

    Update 3477.go

--- 3477.go
+++ 3477.go
@@ -1,6 +1,21 @@
 package main
 
 func main() {
-
+    fmt.Println(numOfUnplacedFruits([1,2,3],[3,2,1]))
 }
 
+func numOfUnplacedFruits(fruits []int, baskets []int) int {
+    cnt := 0
+    for _, fru := range fruits {
+        unst := 1
+        for i:=0;i<len(baskets);i++{
+            if fru <=baskets[i] {
+                baskets[i] = 0
+                unst = 0
+                break
+            }
+        }
+        cnt += unst
+    }
+    return cnt
+}
