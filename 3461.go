e12bea2274b6e00341ba1e116e7c21b7959ebeab
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2025-10-24 02:24:21 +0300

    Create 3461.go

---
+++ 3461.go
@@ -0,0 +1,10 @@
+func hasSameDigits(s string) bool {
+        x := []byte(s)
+        n := len(s)
+        for i:=1; i<=n-2; i++ {
+            for j:=0; j<=n-i-1; j++ {
+                x[j]=byte(((int(x[j]-'0')+int(x[j+1]-'0'))%10) +'0')
+            }
+        }
+        return x[0]==x[1]
+}
