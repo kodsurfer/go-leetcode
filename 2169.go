2df1a95ec5fc87f395acdf77a5d8fdf414f0ce50
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2025-11-09 03:08:46 +0300

    Create 2169.go

---
+++ 2169.go
@@ -0,0 +1,13 @@
+func countOperations(num1 int, num2 int) int {
+    cnt := 0
+    for ; num1>0 && num2>0; {
+        if num1>=num2 {
+            num1 = num1 - num2
+            cnt++
+        } else {
+            num2 = num2 - num1
+            cnt++
+        }
+    }
+    return cnt
+}
